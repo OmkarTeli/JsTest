795f9db24f7d6a631a88c9d07cb00853b68f9915
OmkarTeli <omkarteli8055@gmail.com>
2024-07-30 17:13:36 +0530

    refact/ 2callback.js 3callback.js

--- promises/2callback.js
+++ promises/2callback.js
@@ -1,13 +1,14 @@
+// SOLVING PREVIOUS BY CALLBACKS
 console.log("start");
 
 function importantAction(name, cb) {
-  setTimeout(() => {
-    cb(name);
-  }, 1000);
+   setTimeout(() => {
+      cb(name);
+   }, 1000);
 }
 
 const message = importantAction("subscribe", function (message) {
-  console.log(message);
+   console.log(message);
 });
 
 console.log("stop");
